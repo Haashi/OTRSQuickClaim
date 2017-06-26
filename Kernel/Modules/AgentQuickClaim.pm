f42c868acfcf0fc54dbd7b28b30052aaa731a854
Haashii <baptiste.pontanier@laposte.net>
2017-06-26 17:29:29 +0200

    Add files via upload

--- Kernel/Modules/AgentQuickClaim.pm
+++ Kernel/Modules/AgentQuickClaim.pm
@@ -3,6 +3,12 @@ package Kernel::Modules::AgentQuickClaim;
 use strict;
 use warnings;
 
+our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::Output::HTML::Layout',
+    'Kernel::System::Ticket',
+);
+
 sub new {
     my ( $Type, %Param ) = @_;
 
@@ -10,29 +16,26 @@ sub new {
     my $Self = {%Param};
     bless( $Self, $Type );
 
-    # check needed objects
-    for (qw(ParamObject DBObject LayoutObject LogObject ConfigObject QueueObject TimeObject TicketObject)) {
-        if ( !$Self->{$_} ) {
-            $Self->{LayoutObject}->FatalError( Message => "Got no $_!" );
-        }
-    }
-
     return $Self;
 }
 
 sub Run {
     my ( $Self, %Param ) = @_;
 
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
+
     # check needed stuff
     if ( !$Self->{TicketID} ) {
-        return $Self->{LayoutObject}->ErrorScreen(
+        return $LayoutObject->ErrorScreen(
             Message => 'No TicketID is given!',
             Comment => 'Please contact the admin.',
         );
     }
 
     # check permissions
-    my $Access = $Self->{TicketObject}->TicketPermission(
+    my $Access = $TicketObject->TicketPermission(
         Type     => 'owner',
         TicketID => $Self->{TicketID},
         UserID   => $Self->{UserID}
@@ -40,24 +43,26 @@ sub Run {
 
     # error screen, don't show ticket
     if ( !$Access ) {
-        return $Self->{LayoutObject}->NoPermission(
+        return $LayoutObject->NoPermission(
             Message    => "You need $Self->{Config}->{Permission} permissions!",
             WithHeader => 'yes',
         );
     }
 
-    my $Success = $Self->{TicketObject}->TicketOwnerSet(
+
+    my $Success = $TicketObject->TicketOwnerSet(
         TicketID => $Self->{TicketID},
         UserID   => $Self->{UserID},
         NewUserID => $Self->{UserID},
     );
     if ($Success) {
-        $Self->{TicketObject}->TicketLockSet(
+        $TicketObject->TicketLockSet(
             TicketID => $Self->{TicketID},
             Lock     => 'lock',
             UserID   => $Self->{UserID},
         );
     }
-    return $Self->{LayoutObject}->Redirect( OP => "Action=AgentTicketZoom;TicketID=$Self->{TicketID}" );
+    return $LayoutObject->Redirect( OP => "Action=AgentTicketZoom;TicketID=$Self->{TicketID}" );
 }
+
 1;
