4b2db1397b456d7a501e8965e9c4145bd7112c06
Haashi <baptiste.pontanier@laposte.net>
2017-07-26 16:30:29 +0200

    Update AgentQuickClaim.pm

--- Kernel/Modules/AgentQuickClaim.pm
+++ Kernel/Modules/AgentQuickClaim.pm
@@ -4,6 +4,11 @@ package Kernel::Modules::AgentQuickClaim;
 
 use strict;
 use warnings;
+use Kernel::Config;
+use Kernel::System::Encode;
+use Kernel::System::Log;
+use Kernel::System::Main;
+use Kernel::Language;
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -24,7 +29,32 @@ sub new {
 
 sub Run {
     my ( $Self, %Param ) = @_;
-
+	
+	my $ConfigObject = Kernel::Config->new();
+    my $EncodeObject = Kernel::System::Encode->new(
+        ConfigObject => $ConfigObject,
+    );
+    my $LogObject = Kernel::System::Log->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+    );
+    my $MainObject = Kernel::System::Main->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+    );
+    my $DBObject = Kernel::System::DB->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+    );
+    my $LanguageObject = Kernel::Language->new(
+        MainObject   => $MainObject,
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+    );
     # check needed stuff
     if ( !$Self->{TicketID} ) {
         return $Self->{LayoutObject}->ErrorScreen(
@@ -59,7 +89,23 @@ sub Run {
             Lock     => 'lock',
             UserID   => $Self->{UserID},
         );
+	my $From = "$Self->{UserFirstname} $Self->{UserLastname}";	
+	my $ArticleID = $Self->{TicketObject}->ArticleCreate(
+	        TicketID         => $Self->{TicketID},
+	        ArticleType      => 'note-internal',                        # email-external|email-internal|phone|fax|...
+	        SenderType       => 'agent',                                # agent|system|customer
+	        Subject          => 'QuickClaim : '.$From,		                    # required
+	        From		 => $From,
+		Body             => $From,	                            # required
+	        Charset          => 'ISO-8859-15',
+	        MimeType         => 'text/plain',
+	        HistoryType      => 'OwnerUpdate',                          # EmailCustomer|Move|AddNote|PriorityUpdate|WebRequestCustomer|...
+	        HistoryComment   => 'New owner is '.$From,
+	       	UserID           => $Self->{UserID}
+  	 );
+	
     }
+    
     return $Self->{LayoutObject}->Redirect( OP => "Action=AgentTicketZoom;TicketID=$Self->{TicketID}" );
 }
 1;
